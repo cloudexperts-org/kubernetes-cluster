e83edaa23394b5f77ebc995cc5da258825bdde35
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-24 00:29:45 +0600

    commit39

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "region" {
   type        = string
   description = "AWS region to deploy EKS"
+  default     = "ap-southeast-1"
 }
 
 variable "cluster_name" {
   type        = string
   description = "Name of the EKS cluster"
+  default     = "my-eks-cluster"
 }
