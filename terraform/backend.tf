96a8fea66f9051a69fd7a333121721d315678bff
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-23 16:47:14 +0600

    commit3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,8 +4,5 @@ terraform {
     key            = "eks/terraform.tfstate"
     region         = "ap-southeast-1"
     dynamodb_table = "terraform-lock"
-
-    # Force path style to avoid region redirect issues
-    s3_force_path_style = true
   }
 }
