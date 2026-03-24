e51e64bd8e1a9e6bd78d1e4675e85ed67076de7f
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-24 16:46:00 +0600

    commit18

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,7 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-  #######################################
-  # ✅ NEW AUTH METHOD (v20+)
-  #######################################
-  access_entries = {
+access_entries = {
     github-actions = {
       principal_arn = "arn:aws:iam::865809098262:user/akash"
 
@@ -19,12 +16,9 @@ provider "aws" {
         }
       }
     }
-  }
 }
 
-#######################################
-# EKS Data Sources (for Kubernetes provider)
-#######################################
+
 data "aws_eks_cluster" "cluster" {
   name       = module.eks.cluster_name
   depends_on = [module.eks]
@@ -35,9 +29,6 @@ data "aws_eks_cluster_auth" "cluster" {
   depends_on = [module.eks]
 }
 
-#######################################
-# Kubernetes Provider
-#######################################
 provider "kubernetes" {
   host = module.eks.cluster_endpoint
 
@@ -48,9 +39,6 @@ provider "kubernetes" {
   token = data.aws_eks_cluster_auth.cluster.token
 }
 
-#######################################
-# Outputs
-#######################################
 output "cluster_name" {
   value = module.eks.cluster_name
 }
