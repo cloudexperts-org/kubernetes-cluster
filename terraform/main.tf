be4ca1534854f2283ceb6c581d1360d0be47ca06
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-24 16:41:25 +0600

    commit17

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,52 +2,6 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-#######################################
-# VPC (basic example)
-#######################################
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
-
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["ap-southeast-1a", "ap-southeast-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-}
-
-#######################################
-# EKS Cluster
-#######################################
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.3.0"
-
-  cluster_name    = var.cluster_name
-  cluster_version = "1.35"
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  cluster_endpoint_private_access       = true
-  cluster_endpoint_public_access        = true
-  cluster_endpoint_public_access_cidrs  = ["0.0.0.0/0"]
-
-  enable_irsa = true
-
-  eks_managed_node_groups = {
-    default = {
-      desired_size   = 2
-      min_size       = 1
-      max_size       = 3
-      instance_types = ["t3.medium"]
-    }
-  }
-
   #######################################
   # ✅ NEW AUTH METHOD (v20+)
   #######################################
