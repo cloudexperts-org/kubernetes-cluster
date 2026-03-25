992815ab89025650e0e58be2050e85cb563eb025
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-25 12:04:06 +0600

    commit137

---
+++ argocd.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = "argocd"
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+
+  create_namespace = true
+
+  depends_on = [module.this_eks] 
+}
