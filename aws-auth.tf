064d120bd6fefd5546ed609c009c935608760d2b
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-25 18:43:42 +0600

    commit21

--- aws-auth.tf
+++ aws-auth.tf
@@ -1,6 +1,3 @@
-###############################
-# aws-auth.tf
-###############################
 
 data "aws_eks_cluster" "this" {
   name = module.eks.cluster_name
@@ -14,6 +11,15 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
+}
+
+# Helm provider
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
 }
 
 resource "kubernetes_config_map" "aws_auth" {
