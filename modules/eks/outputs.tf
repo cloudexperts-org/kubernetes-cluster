2a8402e6c30a1dd70c565999a697c467400bd65f
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-26 02:07:02 +0600

    commi22

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -5,10 +5,3 @@ output "cluster_name" {
 output "cluster_arn" {
   value = module.this_eks.cluster_arn
 }
-
-output "managed_node_groups_iam_role_arns" {
-  description = "IAM Role ARNs for all managed node groups"
-  value = {
-    for ng_name, ng in aws_eks_node_group.this : ng_name => ng.iam_role_arn
-  }
-}
