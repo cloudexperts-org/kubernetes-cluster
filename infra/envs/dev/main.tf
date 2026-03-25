6419944bc79791d36f3aae822fc67875a158745c
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-26 00:33:48 +0600

    commit5

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -7,22 +7,11 @@ module "vpc" {
 module "eks" {
   source       = "../../../modules/eks"
   cluster_name = var.cluster_name
-  vpc_id       = module.vpc.vpc_id
   subnet_ids   = module.vpc.public_subnet_ids
+  vpc_id       = module.vpc.vpc_id
 
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
-
-  eks_managed_node_groups = {
-    default = {
-      desired_size   = 1
-      min_size       = 1
-      max_size       = 1
-      instance_types = ["t3.medium"]
-    }
-  }
-
-  enable_irsa = true
 }
 
 module "iam" {
